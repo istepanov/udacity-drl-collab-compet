6fbb33e9b3b56f21aca317a4cc133758a3954236
Ilya Stepanov <dev@ilyastepanov.com>
2019-01-27 13:52:13 -0800

    OU Noise decay + adaptive parameter noise.

--- maddpg_agent.py
+++ maddpg_agent.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 import copy
+from math import sqrt
 from collections import namedtuple, deque
 
 from model import Actor, Critic
@@ -13,11 +14,31 @@ BUFFER_SIZE = int(1e5)  # replay buffer size
 BATCH_SIZE = 128        # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
-LR_ACTOR = 1e-4         # learning rate of the actor 
+LR_ACTOR = 1e-3         # learning rate of the actor
 LR_CRITIC = 1e-3        # learning rate of the critic
 WEIGHT_DECAY = 0        # L2 weight decay
+NOISE_EPSILON_START = 5.0
+NOISE_EPSILON_END = 0.5
+NOISE_EPSILONE_DECAY_LENGTH = 1000
+NOISE_MU=0.0
+NOISE_THETA=0.15
+NOISE_SIGMA=0.2
+LEARN_REPS = 3
+
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+def ddpg_distance_metric(actions1, actions2):
+    """
+    Compute "distance" between actions taken by two policies at the same states
+    Expects numpy arrays
+    """
+    diff = actions1-actions2
+    mean_diff = np.mean(np.square(diff), axis=0)
+    dist = sqrt(np.mean(mean_diff))
+    return dist
+
 
 class Agent():
     """Interacts with and learns from the environment."""
@@ -40,6 +61,7 @@ class Agent():
         # Actor Network (w/ Target Network)
         self.actor_local = Actor(state_size*2, action_size, random_seed).to(device)
         self.actor_target = Actor(state_size*2, action_size, random_seed).to(device)
+        self.actor_perturbed = Actor(state_size*2, action_size, random_seed).to(device)
         self.actor_optimizer = optim.Adam(self.actor_local.parameters(), lr=LR_ACTOR)
 
         # Critic Network (w/ Target Network)
@@ -47,8 +69,16 @@ class Agent():
         self.critic_target = Critic(state_size*2, action_size*2, random_seed).to(device)
         self.critic_optimizer = optim.Adam(self.critic_local.parameters(), lr=LR_CRITIC, weight_decay=WEIGHT_DECAY)
 
+        self.hard_update(self.actor_local, self.actor_target)
+        self.hard_update(self.critic_local, self.critic_target)
+
         # Noise process
         self.noise = OUNoise((num_agents, action_size), random_seed)
+        self.epsilon = NOISE_EPSILON_START
+        self.epsilon_decay = (NOISE_EPSILON_START - NOISE_EPSILON_END) / (NOISE_EPSILONE_DECAY_LENGTH * LEARN_REPS)
+
+        # Parameter noise
+        self.param_noise = AdaptiveParamNoiseSpec(initial_stddev=0.05, desired_action_stddev=0.3, adaptation_coefficient=1.05)
 
         # Replay memory
         self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, random_seed)
@@ -60,20 +90,24 @@ class Agent():
 
         # Learn, if enough samples are available in memory
         if len(self.memory) > BATCH_SIZE:
-            experiences = self.memory.sample()
-            self.learn(experiences, GAMMA, agent_index)
+            for i in range(0, LEARN_REPS):
+                experiences = self.memory.sample()
+                self.learn(experiences, GAMMA, agent_index)
 
-    def act(self, state, add_noise=True):
+    def act(self, state, add_action_noise=False, add_param_noise=False):
         """Returns actions for given state as per current policy."""
         state = torch.from_numpy(state).float().to(device)
         action = np.zeros((self.num_agents, self.action_size))
         self.actor_local.eval()
         with torch.no_grad():
             for i, s in enumerate(state):
-                action[i, :] = self.actor_local(s).cpu().data.numpy()
+                if add_param_noise:
+                    action[i, :] = self.actor_perturbed(s).cpu().data.numpy()
+                else:
+                    action[i, :] = self.actor_local(s).cpu().data.numpy()
         self.actor_local.train()
-        if add_noise:
-            action += self.noise.sample()
+        if add_action_noise:
+            action += self.epsilon * self.noise.sample()
         return np.clip(action, -1, 1)
 
     def reset(self):
@@ -132,6 +166,28 @@ class Agent():
         self.soft_update(self.critic_local, self.critic_target, TAU)
         self.soft_update(self.actor_local, self.actor_target, TAU)
 
+        # update noise decay parameter
+        self.epsilon -= self.epsilon_decay
+        self.epsilon = max(self.epsilon, NOISE_EPSILON_END)
+
+    def perturb_actor_parameters(self):
+        """Apply parameter noise to actor model, for exploration"""
+        self.hard_update(self.actor_local, self.actor_perturbed)
+        params = self.actor_perturbed.state_dict()
+        for name in params:
+            if 'ln' in name: 
+                pass 
+            param = params[name]
+        param += (torch.randn(param.shape) * self.param_noise.current_stddev).to(device)
+
+    def update_param_noise(self, states, actions, next_states):
+        unperturbed_actions = np.zeros((states.shape[0], self.action_size))
+        for i, s in enumerate(states):
+            s = np.expand_dims(s, axis=0)
+            unperturbed_actions[i, :] = self.act(s, add_action_noise=False, add_param_noise=False)
+        ddpg_dist = ddpg_distance_metric(actions, unperturbed_actions)
+        self.param_noise.adapt(ddpg_dist)
+
     def soft_update(self, local_model, target_model, tau):
         """Soft update model parameters.
         θ_target = τ*θ_local + (1 - τ)*θ_target
@@ -145,11 +201,16 @@ class Agent():
         for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
             target_param.data.copy_(tau*local_param.data + (1.0-tau)*target_param.data)
 
+    def hard_update(self, source, target):
+        """ copy weights from source to target network (part of initialization)"""
+        for target_param, param in zip(target.parameters(), source.parameters()):
+            target_param.data.copy_(param.data)
+
 
 class OUNoise:
     """Ornstein-Uhlenbeck process."""
 
-    def __init__(self, size, seed, mu=0., theta=0.15, sigma=0.2):
+    def __init__(self, size, seed, mu=NOISE_MU, theta=NOISE_THETA, sigma=NOISE_SIGMA):
         """Initialize parameters and noise process."""
         self.mu = mu * np.ones(size)
         self.theta = theta
@@ -206,3 +267,39 @@ class ReplayBuffer:
     def __len__(self):
         """Return the current size of internal memory."""
         return len(self.memory)
+
+
+
+class AdaptiveParamNoiseSpec:
+    """
+    From OpenAI Baselines:
+    https://github.com/openai/baselines/blob/master/baselines/ddpg/noise.py
+    """
+    def __init__(self, initial_stddev=0.1, desired_action_stddev=0.2, adaptation_coefficient=1.01):
+        """
+        Note that initial_stddev and current_stddev refer to std of parameter noise, 
+        but desired_action_stddev refers to (as name notes) desired std in action space
+        """
+        self.initial_stddev = initial_stddev
+        self.desired_action_stddev = desired_action_stddev
+        self.adaptation_coefficient = adaptation_coefficient
+
+        self.current_stddev = initial_stddev
+
+    def adapt(self, distance):
+        if distance > self.desired_action_stddev:
+            # Decrease stddev.
+            self.current_stddev /= self.adaptation_coefficient
+        else:
+            # Increase stddev.
+            self.current_stddev *= self.adaptation_coefficient
+
+    def get_stats(self):
+        stats = {
+            'param_noise_stddev': self.current_stddev,
+        }
+        return stats
+
+    def __repr__(self):
+        fmt = 'AdaptiveParamNoiseSpec(initial_stddev={}, desired_action_stddev={}, adaptation_coefficient={})'
+        return fmt.format(self.initial_stddev, self.desired_action_stddev, self.adaptation_coefficient)
